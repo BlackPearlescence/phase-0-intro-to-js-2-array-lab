5e688de6e02db8bd7e7711a6540eabd1db099e3e
Sani Rogers <sanirogers138@gmail.com>
2023-01-10 09:41:37 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+let cats = ["Milo","Otis","Garfield"];
+let copy = [];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    // return cats.splice(cats.leng,0,name);
+    copy = cats.slice();
+    copy.splice(cats.length,0,name);
+    return copy;
+}
+
+function prependCat(name){
+    copy = cats.slice();
+    copy.splice(0,0,name);
+    return copy;
+}
+
+function removeLastCat(){
+    copy = cats.slice();
+    copy.splice(cats.length-1,1);
+    return copy;
+}
+
+function removeFirstCat(){
+    copy = cats.slice();
+    copy.splice(0,1);
+    return copy;
+}
